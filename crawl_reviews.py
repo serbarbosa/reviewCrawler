c43ce8b36189e282ec31dea00c48151f3140917c
sergiobarbosa <sergiobarbosa@usp.br>
2019-08-16 05:15:01 -0300

    nao chama mais scrapy por syscall e sempre substitui o csv

--- crawl_reviews.py
+++ crawl_reviews.py
@@ -1,15 +1,34 @@
+from scrapy.crawler import CrawlerProcess
+from scrapy.utils.project import get_project_settings
 import os
 
-def run_crawler(search):
+def run_crawler(product):
+    #seguindo a documentacao do scrapy ('run Scrapy from a script')
+    
+    csv_filename = 'reviews.csv'
+    
+    #deleta arquivo csv se esse ja existir
+    try:
+        os.remove(csv_filename)
+    except OSError:
+        pass
 
-    os.system('scrapy crawl %s %s %s %s %s %s %s' % ('buscape_crawler', '-s', 'HTTPCASH_ENABLED=1', '-o', 'reviews.csv', '-a', 'search="'+search+'"'))
-    #pool = Pool(processes=len(spider_names))
-    #pool.map(_crawl, spider_names
+    #personalizando configuracoes
+    settings = get_project_settings()
+    settings.set('LOG_ENABLED', False)
+    settings.set('FEED_FORMAT', 'csv')
+    settings.set('FEED_URI', 'reviews.csv')
+    settings.set('ROBOTSTXT_OBEY', False)
+
+    process = CrawlerProcess(settings)
+    process.crawl('buscape_crawler', search=product)
+    process.start()
+    #os.system('scrapy crawl %s %s %s %s %s %s %s' % ('buscape_crawler', '-s', 'HTTPCASH_ENABLED=1', '-o', 'reviews.csv', '-a', 'search="'+search+'"'))
 
 
 if __name__ == "__main__":
     
-    run_crawler('buscape ative')
+    run_crawler('brastemp ative')
     
     
     
--- reviewCrawl/settings.py
+++ reviewCrawl/settings.py
@@ -88,7 +88,7 @@ FEED_EXPORTERS = {
 
 # Enable and configure HTTP caching (disabled by default)
 # See https://doc.scrapy.org/en/latest/topics/downloader-middleware.html#httpcache-middleware-settings
-#HTTPCACHE_ENABLED = True
+HTTPCACHE_ENABLED = True
 #HTTPCACHE_EXPIRATION_SECS = 0
 #HTTPCACHE_DIR = 'httpcache'
 #HTTPCACHE_IGNORE_HTTP_CODES = []
