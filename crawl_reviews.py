196cb283bba185d3565116184fd4ab12cbea8774
sergio <sergiobarbosa@usp.br>
2019-09-21 19:30:25 -0300

    saida padronizada em .json

--- crawl_reviews.py
+++ crawl_reviews.py
@@ -1,26 +1,33 @@
 from scrapy.crawler import CrawlerProcess
 from scrapy.utils.project import get_project_settings
 import os
+import shutil
 
 def run_crawler(product):
     #seguindo a documentacao do scrapy ('run Scrapy from a script')
     
-    csv_filename = 'reviews.csv'
+    json_filename = 'reviews.json'
     
     #deleta arquivo csv se esse ja existir
     try:
-        os.remove(csv_filename)
+        os.remove(json_filename)
     except OSError:
         pass
 
     #personalizando configuracoes
     settings = get_project_settings()
     settings.set('LOG_ENABLED', False)
-    settings.set('FEED_FORMAT', 'csv')
-    settings.set('FEED_URI', 'reviews.csv')
+    settings.set('FEED_FORMAT', 'json')
+    settings.set('FEED_URI', 'reviews.json')
+    settings.set('FEED_EXPORT_ENCODING', 'utf-8')
     settings.set('ROBOTSTXT_OBEY', False)
 
     process = CrawlerProcess(settings)
+
+    #limpando pasta para guardar as novas revisoes
+    shutil.rmtree('reviewsFiles')
+    os.makedirs('reviewsFiles')
+
     process.crawl('buscape_crawler', search=product)
     process.start()
     #os.system('scrapy crawl %s %s %s %s %s %s %s' % ('buscape_crawler', '-s', 'HTTPCASH_ENABLED=1', '-o', 'reviews.csv', '-a', 'search="'+search+'"'))
